7e93d0044aa861eaf7fd8d9ed9281163d6793e90
brarian <b.namonywa@gmail.com>
2017-08-17 19:57:09 -0500

    can't find module error

--- server.js
+++ server.js
@@ -1,9 +1,133 @@
 var express = require('express');
-var app = express();
-app.use(express.static('public'));
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const flash = require('connect-flash');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+dotenv.load();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(process.env.PORT || 8080);
+
 
 app.get('/', function(req, res) {
     res.render('/public');
+});
+
+// This will configure Passport to use Auth0
+const strategy = new Auth0Strategy({
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
+);
+
+passport.use(strategy);
+
+// you can use this section to keep a smaller payload
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+// view engine setup
+var engines = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+    session({
+        secret: 'shhhhhhhhh',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
+// Handle auth failure error messages
+app.use(function(req, res, next) {
+    if (req && req.query && req.query.error) {
+        req.flash("error", req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash("error_description", req.query.error_description);
+    }
+    next();
+});
+
+// Check logged in
+app.use(function(req, res, next) {
+    res.locals.loggedIn = false;
+    if (req.session.passport && typeof req.session.passport.user != 'undefined') {
+        res.locals.loggedIn = true;
+    }
+    next();
+});
+
+app.use('/', routes);
+app.use('/user', user);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+
+
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 let server;
@@ -22,14 +146,15 @@ function runServer() {
 
 function closeServer() {
     return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve();
-        });
+            console.log('Closing server');
+            server.close(err => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve();
+                )
+            });
     });
 }
 
