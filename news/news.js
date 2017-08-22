f735d2984c5d20af5cb9cd68b550137beeda88ac
brarian <b.namonywa@gmail.com>
2017-08-21 21:28:04 -0500

    working design

--- news/news.js
+++ news/news.js
@@ -13,18 +13,52 @@ $(document).ready(function() {
             const title = (response.articles[i].title);
             const url = (response.articles[i].url);
             const desc = (response.articles[i].description);
+            const date = (response.articles[i].publishedAt);
             var pic = ("<img src=" + response.articles[i].urlToImage + "></img>");
             $('.section').append(`<div class="card">
             <div class="text">
-                <div class="writer"> ${author} </div>
-                <div class=" name">${title}</div>
+                <div class="name"><a target="_blank" href="${url}">${title}</a></div>
+                <div class="writer"> ${author}</div>
+                <div class="image"><a target="_blank" href="${url}">${pic}</a></div>
                 <div class="desc">${desc}</div>
             </div>
-            <div class="image">${pic}</div>
         </div>
         `);
         }
 
     });
 
+    var service_url = 'https://newsapi.org/v1/articles?';
+    var params = {
+        status: "ok",
+        source: "ars-technica",
+        sortBy: "top",
+        key: '3abe22b4968b4610833e2fdff4e3e47b'
+    }
+    $.getJSON(service_url + 'source=' + (params.source) + "&sortBy=" + (params.sortBy) + "&apiKey=" + (params.key), function(response) {
+        for (var i = 0; i < response.articles.length; i++) {
+            const author = (response.articles[i].author);
+            const title = (response.articles[i].title);
+            const url = (response.articles[i].url);
+            const desc = (response.articles[i].description);
+            var pic = ("<img src=" + response.articles[i].urlToImage + "></img>");
+            $('.section').append(`<div class="card">
+                <div class="text">
+                    <div class=" name"><a target="_blank" href="${url}">${title}</a></div>
+                    <div class="writer"> ${author}</div>
+                    <div class="image"><a target="_blank" href="${url}">${pic}</a></div>
+                    <div class="desc">${desc}</div>
+                </div>
+            </div>
+            `);
+        }
+
+    });
+
 });
+
+//add Engadget technology / en
+//Hacker News technology / en
+//TechRadar technology / en
+//The Next Web technology / en
+//The Verge technology / en
