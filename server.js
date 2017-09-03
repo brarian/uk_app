be494bc7aa75027580d4a5bf86904b13a6609b8d
brarian <b.namonywa@gmail.com>
2017-09-03 07:46:52 -0500

    errors // out 162-182

--- server.js
+++ server.js
@@ -112,8 +112,8 @@ app.get('/newsfeed', (req, res) => {
 });
 
 //route to the user's personal feed page
-app.get('/favorites', () => {
-    res.render('./personal-feed');
+app.get('/favorites', (req, res) => {
+    res.render('personal-feed.html');
 });
 
 app.get('/login', passport.authenticate('auth0', {
@@ -161,28 +161,25 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.render('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
 
-
-
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 let server;
 
