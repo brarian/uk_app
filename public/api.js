91f1a512969bedb682489cd0a516513eb612eec3
brarian <b.namonywa@gmail.com>
2017-10-07 19:08:54 -0500

    added remaining sources

--- public/api.js
+++ public/api.js
@@ -2,16 +2,21 @@ function getArticles() {
     var service_url = 'https://newsapi.org/v1/articles?';
     const articleJSON = $.getJSON(service_url + "source=" + "techcrunch" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
     const engadgetJSON = $.getJSON(service_url + 'source=' + "engadget" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
-    // const hackerPromise = $.getJSON(service_url + 'source=' + "hacker-news" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
-    // const recodePromise = $.getJSON(service_url + 'source=' + "recode" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
-    // const radarPromise = $.getJSON(service_url + 'source=' + "techradar" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
-    // const vergePromise = $.getJSON(service_url + 'source=' + "the-verge" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
-    // const nextPromise = $.getJSON(service_url + 'source=' + "the-next-web" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
+    const hackerJSON = $.getJSON(service_url + 'source=' + "hacker-news" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
+    const recodeJSON = $.getJSON(service_url + 'source=' + "recode" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
+    const radarJSON = $.getJSON(service_url + 'source=' + "techradar" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
+    const vergeJSON = $.getJSON(service_url + 'source=' + "the-verge" + "&sortBy=" + "top" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
+    const nextJSON = $.getJSON(service_url + 'source=' + "the-next-web" + "&sortBy=" + "latest" + "&apiKey=" + "3abe22b4968b4610833e2fdff4e3e47b");
 
     // return Promise.resolve(articlePromise);
     const articlePromise = Promise.resolve(articleJSON);
     const engadgetPromise = Promise.resolve(engadgetJSON);
-    return Promise.all([articlePromise, engadgetPromise]);
+    const hackerPromise = Promise.resolve(hackerJSON);
+    const recodePromise = Promise.resolve(recodeJSON);
+    const radarPromise = Promise.resolve(radarJSON);
+    const vergePromise = Promise.resolve(vergeJSON);
+    const nextPromise = Promise.resolve(nextJSON);
+    return Promise.all([articlePromise, engadgetPromise, hackerPromise, recodePromise, radarPromise, vergePromise, nextPromise]);
 };
 
 
